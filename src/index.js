2bde0c9cbf3ce76677e43c9ac230b532178c366d
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-01-15 20:10:40 +0100

    step 3 update

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CardList from './CardList'
-import { robots } from './robots';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-    {/* <App /> */}
-    <CardList robots={robots}/>
+    <App />
     </div>
   </React.StrictMode>
 );
