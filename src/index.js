029402981ae216b4d4a282bed4b98f7abe37379d
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-01-15 14:20:19 +0100

    Step 2 concluded

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
+import CardList from './CardList'
 import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,9 +11,7 @@ root.render(
   <React.StrictMode>
     <div>
     {/* <App /> */}
-    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
+    <CardList robots={robots}/>
     </div>
   </React.StrictMode>
 );
